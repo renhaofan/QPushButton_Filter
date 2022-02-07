70d67ee6e31f81a5203b319879f40cb4364159f7
renhaofan <yqykrhf@163.com>
2022-02-07 17:59:15 +0800

    first commit

---
+++ QNeumorphism.h
@@ -0,0 +1,41 @@
+#ifndef QNeumorphism_h__
+#define QNeumorphism_h__
+
+#include <QGraphicsEffect>
+#include <QtMath>
+
+class QNeumorphism : public QGraphicsEffect
+{
+public:
+    QNeumorphism(qreal blurRadius = 10, qreal distance = 3, qreal strength = 0.8, qreal angle = 45, bool inset = false);
+    ~QNeumorphism();
+
+    qreal strength() const;
+    void setStrength(const qreal& strength);
+
+    qreal blurRadius() const;
+    void setBlurRadius(const qreal& blurRadius);
+
+    qreal distance() const;
+    void setDistance(const qreal& distance);
+
+    qreal angle() const;
+    void setAngle(const qreal& angle);
+
+    bool inset() const;
+    void setInset(bool inset);
+
+
+protected:
+    QRectF boundingRectFor(const QRectF& rect) const override;
+    void draw(QPainter* painter) override;
+
+private:
+    qreal blurRadius_;
+    qreal distance_;
+    qreal strength_;
+    qreal angle_;
+    bool inset_;
+};
+
+#endif // QNeumorphism_h__
---
+++ main.cpp
@@ -0,0 +1,13 @@
+// https://neumorphism.io 作者：-italink- https://www.bilibili.com/read/cv13418947
+
+
+#include "Widget.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Widget w;
+    w.show();
+    return a.exec();
+}
